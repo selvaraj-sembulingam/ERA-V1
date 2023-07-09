a909dc554677c863109da788f0a6b08d3a316fda
Selvaraj Sembulingam <selvaskingdom@gmail.com>
2023-07-09 23:49:20 +0530

    Update train.py

--- Assignments/S10/train.py
+++ Assignments/S10/train.py
@@ -6,7 +6,6 @@ from albumentations.pytorch import ToTensorV2
 from torch.optim.lr_scheduler import OneCycleLR
 from torchvision import transforms
 from torch_lr_finder import LRFinder
-import matplotlib
 
 
 # Setup hyperparameters
@@ -61,6 +60,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=
 lr_finder = LRFinder(model, optimizer, criterion, device="cuda")
 lr_finder.range_test(train_dataloader, end_lr=10, num_iter=200, step_mode="exp")
 lr_finder.plot() # to inspect the loss-learning rate graph
+plt.savefig("results/lr_finder_plot.png")
 lr_finder.reset() # to reset the model and optimizer to their initial state
 
 scheduler = OneCycleLR(
