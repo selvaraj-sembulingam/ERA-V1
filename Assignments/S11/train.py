8ed090f2acffab694dcdabe01c9d985e37a29981
Selvaraj Sembulingam <selvaskingdom@gmail.com>
2023-07-27 21:46:57 +0530

    Update train.py

--- Assignments/S11/train.py
+++ Assignments/S11/train.py
@@ -33,8 +33,8 @@ if not os.path.exists("results"):
 train_transforms = A.Compose([
     A.PadIfNeeded(min_height=32 + 4, min_width=32 + 4, p=1),
     A.RandomCrop(32, 32),
-    A.HorizontalFlip(),
-    A.CoarseDropout(max_holes=1, max_height=8, max_width=8, min_holes=1, min_height=8, min_width=8, fill_value=(0.49139968, 0.48215827, 0.44653124), p=1),  # Apply coarse dropout
+    #A.HorizontalFlip(),
+    A.CoarseDropout(max_holes=1, max_height=16, max_width=16, min_holes=1, min_height=16, min_width=16, fill_value=(0.49139968, 0.48215827, 0.44653124), p=1),  # Apply coarse dropout
     A.Normalize(mean=[0.49139968, 0.48215827, 0.44653124], std=[0.24703233, 0.24348505, 0.26158768]),  # Normalize the image
     ToTensorV2() # Convert image to a PyTorch tensor
 ])
