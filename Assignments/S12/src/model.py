3dbba0f5ca03d9629c093c10eefe6c500250bd08
Selvaraj Sembulingam <selvaskingdom@gmail.com>
2023-08-04 19:10:37 +0530

    Update model.py

--- Assignments/S12/src/model.py
+++ Assignments/S12/src/model.py
@@ -136,7 +136,7 @@ class CustomResNet(pl.LightningModule):
 
         return [optimizer], [scheduler]
         
-    def on_train_batch_end(self, outputs, batch, batch_idx, dataloader_idx):
+    def on_train_batch_end(self, batch, batch_idx):
         metrics = self.trainer.callback_metrics
         logger.info(f'{metrics}')
     
