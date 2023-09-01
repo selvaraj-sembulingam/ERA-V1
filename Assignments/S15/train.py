df5933101cd093fb1b7e01b99e9d6f9d1ae46c56
Selvaraj Sembulingam <selvaskingdom@gmail.com>
2023-09-01 12:37:48 +0530

    Update train.py

--- Assignments/S15/train.py
+++ Assignments/S15/train.py
@@ -158,7 +158,7 @@ def get_or_build_tokenizer(config, ds, lang):
     tokenizer_path = Path(config["tokenizer_file"].format(lang))
     if not Path.exists(tokenizer_path):
         tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
-        tokenizer.pre_tokenizers = Whitespace()
+        tokenizer.pre_tokenizer = Whitespace()
         trainer = WordLevelTrainer(
             special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2
         )
