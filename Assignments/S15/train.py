6e22769bff8e62608f08532f0746c47c22638b49
Selvaraj Sembulingam <selvaskingdom@gmail.com>
2023-09-01 12:33:36 +0530

    Update train.py

--- Assignments/S15/train.py
+++ Assignments/S15/train.py
@@ -265,7 +265,7 @@ def train_model(config):
 
     loss_fn = nn.CrossEntropyLoss(
         ignore_index=tokenizer_src.token_to_id("[PAD]"), label_smoothing=0.1
-    )
+    ).to(device)
 
     for epoch in range(initial_epoch, config["num_epochs"]):
         torch.cuda.empty_cache()
