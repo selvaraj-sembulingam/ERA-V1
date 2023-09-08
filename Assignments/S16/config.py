7198085cdd9827ede2325f3228b80b3b269cb644
Selvaraj Sembulingam <selvaskingdom@gmail.com>
2023-09-08 22:46:34 +0530

    Add files via upload

---
+++ Assignments/S16/config.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+
+def get_config():
+    return {
+        "batch_size": 32,
+        "num_epochs": 50,
+        "lr": 10**-3,
+        "seq_len": 180,
+        "d_model": 512,
+        "lang_src": "en",
+        "lang_tgt": "fr",
+        "model_folder": "weights",
+        "model_basename": "tmodel_",
+        "preload": False,
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/tmodel",
+    }
+
+
+def get_weights_file_path(config, epoch: str):
+    model_folder = config["model_folder"]
+    model_basename = config["model_basename"]
+    model_filename = f"{model_basename}{epoch}.pt"
+    return str(Path(".") / model_folder / model_filename)
